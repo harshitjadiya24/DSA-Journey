d7dc32b5689cda58fa56334e344544c83906fab4
Harshit Jadiya <=>
2026-05-20 20:33:58 +0530

    second commit

---
+++ DSA-Journey/Day1/linearSearch.cpp
@@ -0,0 +1,26 @@
+// find the index of largest and smallest element
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int sizeOfArray;
+  int target = 8;
+  int index1;
+  cin >> sizeOfArray;
+  int arr[sizeOfArray];
+
+  for (int i = 0; i < sizeOfArray; i++)
+  {
+    cin >> arr[i];
+  }
+  for (int i = 0; i < sizeOfArray; i++)
+  {
+    if (arr[i] == target)
+    {
+      index1 = i;
+    }
+  }
+  cout << "Target number index is " << index1 << endl;
+}
